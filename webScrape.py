af58acfc9f5e03ac667591250b9886a19c1c03e1
INeedCat <97716183+INeedCat@users.noreply.github.com>
2023-11-29 10:30:00 -0700

    Update webScrape.py
    Updated JavaScript to disallow scrolling to the left of first page

--- webScrape.py
+++ webScrape.py
@@ -95,18 +95,36 @@ writetofile.write("""
 </body>
 <script>
 let page = document.querySelector('html');
-let dist =  1390/* 1398 if fullscreen*/;
+let dist =  1398/* 1398 if fullscreen, else 1390*/;
 
-document.addEventListener('keydown', (e) => {
-    if (e.keyCode == 39){
-        e.preventDefault();
+let pagesInBook = 600; //Eragon:294, Inheritance:515, Eldest:397, Brisingr:
+
+if(localStorage.pageNum > 0){
+
+    for(let i = 1; i < Number(localStorage.pageNum); i++){
         page.style.left = parseFloat(page.style.left || 0) - dist + 'px'; 
         page.style.right = parseFloat(page.style.right || 0) + dist + 'px';
     }
-    else if (e.keyCode == 37){
+
+} else {
+    localStorage.pageNum = 1;
+}
+
+
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode == 39 && Number(localStorage.pageNum) < pagesInBook){
+        e.preventDefault();
+        page.style.left = parseFloat(page.style.left || 0) - dist + 'px'; 
+        page.style.right = parseFloat(page.style.right || 0) + dist + 'px';
+
+        localStorage.pageNum = Number(localStorage.pageNum) + 1;
+    }
+    else if (e.keyCode == 37 && Number(localStorage.pageNum) > 1){
         e.preventDefault();
         page.style.left = parseFloat(page.style.left || 0) + dist + 'px'; 
         page.style.right = parseFloat(page.style.right || 0) - dist + 'px';
+
+        localStorage.pageNum = Number(localStorage.pageNum) - 1;
     }
 });
 </script>
